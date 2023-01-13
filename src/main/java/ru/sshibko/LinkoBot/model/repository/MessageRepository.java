c4940102221686a4df13485f29c06a2f0e88890f
Serge <sshibkojavadev@gmail.com>
2023-01-13 09:59:37 +0800

    some fixes and functional addings

--- src/main/java/ru/sshibko/LinkoBot/model/repository/MessageRepository.java
+++ src/main/java/ru/sshibko/LinkoBot/model/repository/MessageRepository.java
@@ -10,12 +10,18 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    String query0 = "SELECT * FROM message WHERE "
+            + "user_id =:chatId LIMIT 10";
+
     String query1 = "SELECT * FROM message  WHERE "
             + "description LIKE %:keyword%"
-            + " OR link LIKE %:keyword%";
+            + " OR link LIKE %:keyword% LIMIT 10";
 
-    String query33 = "SELECT * FROM message WHERE link LIKE %:keyword%";
-    String query34 = "SELECT * FROM message WHERE link LIKE %?1%";
+    String query3 = "SELECT * FROM message WHERE link LIKE %:keyword%";
+    String query4 = "SELECT * FROM message WHERE link LIKE %?1%";
     @Query(value = query1, nativeQuery = true)
-    public List<Message> search(String keyword);
+    List<Message> search(String keyword);
+
+    @Query(value = query0, nativeQuery = true)
+    List<Message> findAllChatIdMessages(Long chatId);
 }
--- src/main/java/ru/sshibko/LinkoBot/service/LinkoBotService.java
+++ src/main/java/ru/sshibko/LinkoBot/service/LinkoBotService.java
@@ -1,7 +1,6 @@
 package ru.sshibko.LinkoBot.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
@@ -44,8 +43,9 @@ public class LinkoBotService extends TelegramLongPollingBot {
     @Value("${success.report.switch}")
     private static byte successReport;
 
-    private final static String INVITE_TEXT = ", hello! I’ll help you store and sort the links you need." +
-            " Please, register before you start sending your links";
+    private final static String INVITE_TEXT = ", hello! I’ll help you store and sort the links you need. \n" +
+            " Please, register before you start sending your links with command `/register` \n" +
+            " Or get help with command `/help`";
 
     private static final String HELP_TEXT = "This bot is designed for retrieving, storing and sorting user links to internet resources\n\n"
             + "You can execute commands from the main menu at the left angle or by typing command:\n\n"
@@ -58,7 +58,7 @@ public class LinkoBotService extends TelegramLongPollingBot {
             + "Type /getall to get all links (not recommended)\n\n"
             + "Type /gettimed to get links sorted by received time\n\n"
             + "Type /getab to get links sorted by alphabet\n\n"
-            + "Type /";
+            + "Type any other words or letters to find link by entered keywords";
 
     @Autowired
     public LinkoBotService(BotConfig config) {
@@ -69,7 +69,6 @@ public class LinkoBotService extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error("Error setting bot command list: " + e.getMessage());
         }
-
     }
 
     @Override
@@ -87,13 +86,13 @@ public class LinkoBotService extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-            if (messageText.contains("http") && chatId == config.getOwnerChatId()) {
+            if (messageText.contains("http")) {
                 saveMessage(chatId, messageText);
                 deleteMessage(chatId, update);
 
                 log.info("user " + messageText + " saved");
             } else {
-                List<Message> messages = messageRepository.findAll();
+                List<Message> messages = messageRepository.findAllChatIdMessages(chatId);
                 switch (messageText) {
                     case "/start":
                         sendMessage(chatId, userRepository.findById(chatId).orElseThrow().getFirstName() + INVITE_TEXT);
@@ -122,6 +121,29 @@ public class LinkoBotService extends TelegramLongPollingBot {
                         messages.sort(Comparator.comparing(Message::getLink));
                         for (Message message : messages) {
                             getMyLinks(chatId, message.getLink(), message.getDescription());
+                        }
+                        break;
+                    case "/find":
+                        sendMessage(chatId, "Please, enter keyword to find");
+                        List<Message> messageFinds = findMessages(update, messages);
+                        if (messageFinds == null) {
+                            sendMessage(chatId, "No results found");
+                        } else {
+                            for (Message message : messageFinds) {
+                                getMyLinks(chatId, message.getLink(), message.getDescription());
+                            }
+                        }
+                        break;
+                    default:
+                        //sendMessage(chatId, "Sorry, command was not recognized");
+                        //For find case:
+                        List<Message> messageList = findMessages(update, messages);
+                        if (messageList == null) {
+                            sendMessage(chatId, "No results found");
+                        } else {
+                            for (Message message : messageList) {
+                                getMyLinks(chatId, message.getLink(), message.getDescription());
+                            }
                         }
                         break;
                 }
@@ -161,6 +183,9 @@ public class LinkoBotService extends TelegramLongPollingBot {
         commandList.add(new BotCommand("/help", "help info"));
         commandList.add(new BotCommand("/settings", "set your preferences"));
         commandList.add(new BotCommand("/register", "for registration"));
+        commandList.add(new BotCommand("/gettimed", "for links sorted by time"));
+        commandList.add(new BotCommand("/getab", "for links sorted by alphabet"));
+        commandList.add(new BotCommand("/getall", "for all links BE CAREFULL!"));
 
         return commandList;
     }
@@ -217,5 +242,15 @@ public class LinkoBotService extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error(LOG_ERROR_TEXT + e.getMessage());
         }
+    }
+
+    private List<Message> findMessages(Update update, List<Message> messages) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            messages = messageRepository.search(messageText);
+            return messages;
+        }
+        return null;
     }
 }
